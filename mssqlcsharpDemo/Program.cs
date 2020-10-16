c6b9e88ecfbe1d4a74468dbe8f9e043c9454e2de
Alley <alley.borden@go.tarleton.edu>
2020-10-16 16:30:35 -0500

    commit 1

--- mssqlcsharpDemo/Program.cs
+++ mssqlcsharpDemo/Program.cs
@@ -1,13 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
-namespace mssqlcsharpDemo {
+namespace SQLdemoCsharp {
     class Program {
         static void Main(string[] args)
         {
+
+            string connetionString = null;
+            SqlConnection cnn;
+
+            connetionString = "Data Source=cstnt.tstc.edu;Initial Catalog=inew2330fa20;User ID=aborden;Password=password";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                Console.WriteLine("Connection Open ! ");
+                while (true)
+                {
+                    try
+                    {
+                        string query = "";
+                        Console.WriteLine("Enter Query: ");
+                        query = Console.ReadLine();
+                        if (query == "quit")
+                        {
+                            Console.WriteLine("Operations terminated.\n..\n...");
+                            return;
+                        }
+                        SqlCommand command = new SqlCommand(query, cnn);
+                        SqlDataReader dataReader = command.ExecuteReader();
+
+                        while (dataReader.Read())
+                        {
+                            for (int i = 0; i < dataReader.FieldCount; i++)
+                            {
+                                Console.Write(dataReader[i] + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        dataReader.Close();
+                    }
+                    catch (SqlException x)
+                    {
+                        Console.WriteLine(x.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            cnn.Close();
+
         }
     }
 }
