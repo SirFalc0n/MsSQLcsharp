b3f88f3b6c82c4fa3bb75556286e150dc9c35568
Dave <david.kuehne@tstc.edu>
2020-10-19 14:54:32 -0500

    Changed output message.

--- mssqlcsharpDemo/Program.cs
+++ mssqlcsharpDemo/Program.cs
@@ -24,7 +24,7 @@ namespace SQLdemoCsharp {
                         query = Console.ReadLine();
                         if (query == "quit")
                         {
-                            Console.WriteLine("Operations terminated.\n..\n...");
+                            Console.WriteLine("Operations has ended.\n..\n...");
                             return;
                         }
                         SqlCommand command = new SqlCommand(query, cnn);
